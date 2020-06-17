f5484cf5c3b5f62772518dd350a7f84f8b2aa00d
Rodrigo Arias <rodarima@gmail.com>
2020-06-17 13:26:14 +0200

    Clean unused derivations

--- default.nix
+++ default.nix
@@ -3,7 +3,6 @@
 let
   inherit (pkgs.lib) callPackageWith;
   inherit (pkgs.lib) callPackagesWith;
-  #callPackage = callPackageWith (pkgs);
   callPackage = callPackageWith (pkgs // self.bsc);
   callPackages = callPackagesWith (pkgs // self.bsc);
 
@@ -48,83 +47,11 @@ let
       extrae = extrae;
     };
 
-#
-#    clang-ompss2 = import ./bsc/cc-wrapper/default.nix {
-##      inherit stdenv binutils coreutils ;
-##      stdenv = bsc.stdenv;
-#      coreutils = pkgs.coreutils;
-#      bintools = pkgs.binutils;
-#      gnugrep = pkgs.gnugrep;
-#      stdenvNoCC = pkgs.stdenvNoCC;
-#      libc = pkgs.glibc;
-#      nativeTools = false;
-#      nativeLibc = false;
-#      cc = clang-ompss2-unwrapped;
-#    };
-
-
-    
-
-#    gcc = lib.makeOverridable (import ./bsc/cc-wrapper/default.nix) {
-#      nativeTools = false;
-#      nativeLibc = false;
-#      isGNU = true;
-#      buildPackages = {
-#        inherit (prevStage) stdenv;
-#      };
-#      cc = prevStage.gcc-unwrapped;
-#      bintools = self.binutils;
-#      libc = getLibc self;
-#      inherit (self) stdenvNoCC coreutils gnugrep;
-#      shell = self.bash + "/bin/bash";
-#    };
-
-    #stdenv = stdenvClangOmpss;
-
-#    WrappedICC = import ../patches/cc-wrapper  {
-#      inherit stdenv binutils coreutils ;
-#      libc = glibc;
-#      nativeTools = false;
-#      nativeLibc = false;
-#      cc = icc-native;
-#    };
-#
-#
-#    stdenvICC = (overrideCC stdenv WrappedICC) // {  isICC = true; };
-#
-#    stdenvIntelfSupported = if (WrappedICC != null) then stdenvICC else stdenv;
-#
-#    stdenvIntelIfSupportedElseClang = if (WrappedICC != null) then stdenvICC else clangStdenv;
-#
-#    intelMKLIfSupported = if (WrappedICC != null) then intel-mkl else pkgs.blas;
-#  };
-
-#    llvmPackages_10 = callPackage ../development/compilers/llvm/10 ({
-#      inherit (stdenvAdapters) overrideCC;
-#      buildLlvmTools = buildPackages.llvmPackages_10.tools;
-#      targetLlvmLibraries = targetPackages.llvmPackages_10.libraries;
-#    } // stdenv.lib.optionalAttrs (stdenv.hostPlatform.isi686 && buildPackages.stdenv.cc.isGNU) {
-#      stdenv = gcc7Stdenv;
-#    });
-
-#    llvmPackages_latest = llvmPackages_10;
-
-#
-#    cpic = callPackage ./bsc/cpic/default.nix {
-#      stdenv = stdenv_nanos6;
-#      tampi = tampi;
-#      mpi = mpi;
-#      nanos6 = nanos6-git;
-##      llvm-ompss2 = llvm-ompss2;
-#    };
-
     dummy = callPackage ./bsc/dummy/default.nix {
     };
 
-    chroot = callPackage ./test/chroot.nix {};
-
-#    llvmOmpss2Packages = callPackage bsc/llvm-ompss2/11/default.nix {};
-
+    chroot = callPackage ./test/chroot.nix {
+    };
 
     clang-ompss2-unwrapped = callPackage ./bsc/llvm-ompss2/clang.nix {
       stdenv = pkgs.llvmPackages_10.stdenv;
@@ -147,10 +74,8 @@ let
 
     cpic = callPackage ./bsc/cpic/default.nix {
       stdenv = stdenv-nanos6;
-      tampi = tampi;
-      mpi = mpi;
       nanos6 = nanos6-git;
-#      llvm-ompss2 = llvm-ompss2;
+      inherit mpi tampi;
     };
 
   };
