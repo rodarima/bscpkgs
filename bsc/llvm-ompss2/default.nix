d9ec42614c2d74dcf896091a749716b16b2c5f40
Rodrigo Arias <rodarima@gmail.com>
2020-06-17 13:21:44 +0200

    Fix libstdc++.so path

--- bsc/llvm-ompss2/default.nix
+++ bsc/llvm-ompss2/default.nix
@@ -18,7 +18,7 @@ in wrapCCWith rec {
     echo "-target ${targetConfig}" >> $out/nix-support/cc-cflags
     echo "-B${gcc.cc}/lib/gcc/${targetConfig}/${gcc.version}" >> $out/nix-support/cc-cflags
     echo "-L${gcc.cc}/lib/gcc/${targetConfig}/${gcc.version}" >> $out/nix-support/cc-ldflags
-    echo "-L${gcc.cc.lib}/${targetConfig}/lib" >> $out/nix-support/cc-ldflags
+    echo "-L${gcc.cc.lib}/lib" >> $out/nix-support/cc-ldflags
     echo "--gcc-toolchain=${gcc}" >> $out/nix-support/cc-cflags
 
     echo "# Hack to include NANOS6_HOME" >> $out/nix-support/setup-hook
