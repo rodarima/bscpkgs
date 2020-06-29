9a5759c45e3bff5ba7126f3be75ace6915735b68
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-06-29 16:41:17 +0200

    Update nanos6-git version

--- bsc/nanos6/git.nix
+++ bsc/nanos6/git.nix
@@ -16,13 +16,13 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "nanos6";
-  version = "2.4-nix-526b0e14";
+  version = "2.4+nix_526b0e14";
   branch = "master";
   cacheline-width = "64";
 
   src = builtins.fetchGit {
     url = "ssh://git@bscpm02.bsc.es/rarias/nanos6";
-    rev = "526b0e1418e445115d79cca402a387a88ea61bb9";
+    rev = "a8372abf9fc7cbc2db0778de80512ad4af244c29";
     ref = branch;
   };
 
