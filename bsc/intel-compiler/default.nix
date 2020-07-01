9d65f2ae2c10450a7ebe95947541cabd2e6751d6
Rodrigo Arias <rodrigo.arias@bsc.es>
2020-07-01 13:08:05 +0200

    Add icc bin to out dir

--- bsc/intel-compiler/default.nix
+++ bsc/intel-compiler/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, rpmextract
 }:
 
 stdenv.mkDerivation rec {
@@ -8,10 +9,32 @@ stdenv.mkDerivation rec {
 
   # From Arch Linux PKGBUILD
   dir_nr="16526";
-  tgz="parallel_studio_xe_2020_update1_cluster_edition.tgz";
+  year="2020";
+  v_a="1";
+  v_b="217";
+  update="1";
+  composer_xe_dir="compilers_and_libraries_${year}.${v_a}.${v_b}";
+  tgz="parallel_studio_xe_2020_update${update}_cluster_edition.tgz";
   
+  # sha256-/RHY3nKyvWBHT4vOe0Y+TLsiVZabnq8k9olXWqKiq6s=
   src = fetchurl {
     url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/${dir_nr}/${tgz}";
-    sha256 = "01wwmiqff5lad7cdi8i57bs3kiphpjfv52sxll1w0jpq4c03nf4h";
+    sha256 = "1axblai5lmw9yqjaz7lvjraj5fsc7r37pklb9x3n1gdjfbgdh4gx";
   };
+
+  buildInputs = [
+    rpmextract
+  ];
+
+  installPhase = ''
+    rpmextract rpm/intel-icc-*.rpm
+    rpmextract rpm/intel-comp-*.rpm
+
+    mkdir -p $out/{bin,lib,include}
+
+    pushd ./opt/intel/${composer_xe_dir}/linux/
+      cp -a bin/intel64/* $out/bin/
+      cp -a compiler/include/* $out/include/
+    popd
+  '';
 }
