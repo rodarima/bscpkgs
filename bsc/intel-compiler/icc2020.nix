419418781fcd8b20660916c4775dc91929f03c3d
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-07-23 19:10:37 +0200

    Revert "icc: use fetchTarball"
    This reverts commit 215b104174e7bdcf9dfe6727683261eea54d036f.

--- bsc/intel-compiler/icc2020.nix
+++ bsc/intel-compiler/icc2020.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchurl
 , rpmextract
 , autoPatchelfHook
 , gcc
@@ -18,9 +19,9 @@ stdenv.mkDerivation rec {
   composer_xe_dir="compilers_and_libraries_${year}.${v_a}.${v_b}";
   tgz="parallel_studio_xe_2020_update${update}_cluster_edition.tgz";
   
-  src = builtins.fetchTarball {
+  src = fetchurl {
     url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/${dir_nr}/${tgz}";
-    sha256 = "1yg2q01ra37ywd7mmgcvkxpsvjz31af91ibybndxn2s0fz7lxibp";
+    sha256 = "1axblai5lmw9yqjaz7lvjraj5fsc7r37pklb9x3n1gdjfbgdh4gx";
   };
 
   buildInputs = [
