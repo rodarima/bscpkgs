9cba2d609c0fddb8dab7722d8b01af098071f3e1
Rodrigo Arias <rodrigo.arias@bsc.es>
2020-07-24 15:30:28 +0200

    Working proof of concept for garlic experiments

--- bsc/garlic/default.nix
+++ bsc/garlic/default.nix
@@ -7,8 +7,18 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // bsc // garlic);
   callPackages = pkgs.lib.callPackagesWith (pkgs // bsc // garlic);
   garlic = rec {
-    mpptest = callPackage ./mpptest/default.nix { };
-    ppong = callPackage ./ppong/default.nix { };
+
+    mpptest = callPackage ./mpptest { };
+
+    ppong = callPackage ./ppong { };
+
+    experiments = callPackage ./experiments {
+      apps = [
+        (ppong.override { mpi=bsc.intel-mpi;})
+        (ppong.override { mpi=pkgs.mpich;})
+      ];
+    };
+
   };
 in
   garlic
---
+++ bsc/garlic/experiments/default.nix
@@ -0,0 +1,39 @@
+{
+  stdenv
+, mpi
+, fetchurl
+, apps
+}:
+
+stdenv.mkDerivation {
+  name = "garlic-experiments";
+
+  src = ./.;
+
+  buildInputs = [] ++ apps;
+  apps = apps;
+
+  buildPhase = ''
+    for app in $apps; do
+      test -e $app/bin/run || (echo $app/bin/run not found; exit 1)
+    done
+  '';
+
+  installPhase = ''
+    mkdir -p $out/apps
+    for app in $apps; do
+      ln -s $app $out/apps/$(basename $app)
+    done
+
+    mkdir -p $out/bin
+    cat > $out/bin/run <<EOF
+    #!/bin/bash
+
+    for app in $out/apps/*; do
+      \$app/bin/run
+    done
+    EOF
+
+    chmod +x $out/bin/run
+  '';
+}
