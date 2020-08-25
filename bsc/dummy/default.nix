cff653d16486c09250267fe55eeb3b6dd4ff18d2
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-08-25 14:52:18 +0200

    Simplify dummy

--- bsc/dummy/default.nix
+++ bsc/dummy/default.nix
@@ -2,23 +2,21 @@
   stdenv
 }:
 
-{
-    hello = stdenv.mkDerivation rec {
-      name = "dummy";
+stdenv.mkDerivation rec {
+  name = "dummy";
 
-      src = null;
-      dontUnpack = true;
-      dontBuild = true;
+  src = null;
+  dontUnpack = true;
+  dontBuild = true;
 
-      installPhase = ''
-        mkdir -p $out/bin
+  installPhase = ''
+    mkdir -p $out/bin
 
-        cat > $out/bin/dummy <<EOF
-        #!/bin/sh
-        echo Hello world!
-        EOF
+    cat > $out/bin/dummy <<EOF
+    #!/bin/sh
+    echo Hello world!
+    EOF
 
-        chmod +x $out/bin/dummy
-      '';
-    };
+    chmod +x $out/bin/dummy
+  '';
 }
