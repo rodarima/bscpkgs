87809ef903dbef7c1c33612f134a50dd355fdcf7
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-08-26 19:20:17 +0200

    Update extrae and enable man pages

--- bsc/extrae/default.nix
+++ bsc/extrae/default.nix
@@ -1,5 +1,5 @@
 { stdenv
-, fetchgit
+, fetchFromGitHub
 , boost
 , libdwarf
 , libelf
@@ -10,29 +10,36 @@
 , libiberty
 , gfortran
 , xml2
+, which
 , mpi ? null
 , cuda ? null
 , llvmPackages
 , autoreconfHook
+, python37Packages
+, installShellFiles
 }:
 
 stdenv.mkDerivation rec {
   name = "extrae";
-  version = "3.7.1";
+  version = "3.8.3";
 
 #  src = fetchurl {
 #    url = "https://ftp.tools.bsc.es/extrae/${name}-${version}-src.tar.bz2";
 #    sha256 = "0y036qc7y30pfj1mnb9nzv2vmxy6xxiy4pgfci6l3jc0lccdsgf8";
 #  };
 
-  # Use patched Extrae version
-  src = fetchgit {
-    url = "https://github.com/rodarima/extrae";
-    rev = "15883516d6bd802e5b76ff28c4b4a3a5cb113880";
-    sha256 = "1hmf6400kw5k3j6xdbbd0yw4xhrjhk1kibp6m7r2i000qjgha8v6";
+  src = fetchFromGitHub {
+    owner = "rodarima";
+    #owner = "bsc-performance-tools";
+    repo = "extrae";
+    rev = "a8ec6882c03d130f88b09f2114887101ca9f6b09";
+    #rev = "${version}";
+    sha256 = "02gwl17r63kica6lxycyn10a0r2ciycf6g3cdq5cna5zl351qf31";
   };
 
   enableParallelBuilding = true;
+
+  nativeBuildInputs = [ installShellFiles ];
 
   buildInputs = [
     autoreconfHook
@@ -44,7 +51,9 @@ stdenv.mkDerivation rec {
     libiberty
     mpi
     xml2
+    which
     libxml2.dev
+    python37Packages.sphinx
   ]
   ++ stdenv.lib.optional stdenv.cc.isClang llvmPackages.openmp;
     
@@ -69,6 +78,16 @@ stdenv.mkDerivation rec {
       ${if (mpi != null) then ''--with-mpi=${mpi}''
         else ''--without-mpi''}
       --without-dyninst)
+  '';
+
+  # Install the manuals only by hand, as we don't want to pull the complete
+  # LaTeX world
+  postBuild = ''
+    make -C docs man
+  '';
+
+  postInstall = ''
+    installManPage docs/builds/man/*/*
   '';
 
 #  ++ (
