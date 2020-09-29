eb46e8f41bc9824c83a85e406db34f4e482d8101
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-09-29 16:55:14 +0200

    tampi: Disable the C++ MPI interface for OpenMPI
    Fixes #30

--- bsc/tampi/default.nix
+++ bsc/tampi/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
   buildInputs = [ autoreconfHook automake autoconf libtool gnumake boost mpi gcc ];
   dontDisableStatic = true;
-  configureFlags = [ "--disable-mpi-mt-check" ];
+  configureFlags = [ "--disable-mpi-mt-check" "CXXFLAGS=-DOMPI_SKIP_MPICXX=1" ];
   src = fetchurl {
     url = "https://github.com/bsc-pm/tampi/archive/v${version}.tar.gz";
     sha256 = "8608a74325939d2a6b56e82f7f6788efbc67731e2d64793bac69475f5b4b9704";
