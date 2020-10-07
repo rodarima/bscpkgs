66a5e06adab0553686038aa47ebceb4502aa81b6
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-10-07 18:41:21 +0200

    Generate trebuchet from nix

--- garlic/stages/trebuchet/default.nix
+++ garlic/stages/trebuchet/default.nix
@@ -12,14 +12,23 @@
 
 stdenv.mkDerivation {
   name = "trebuchet";
+  phases = [ "installPhase" ];
   preferLocalBuild = true;
-  phases = [ "unpackPhase" "installPhase" ];
   dontPatchShebangs = true;
-  src = ./.;
-  inherit sshHost nixPrefix nixtools targetCluster program;
   installPhase = ''
-    substituteAllInPlace trebuchet
-    cp trebuchet $out
+    cat > $out <<EOF
+    #!/bin/sh -e
+    # Using the token @upload-to-mn@ we instruct the post-build hook to upload
+    # this script and it's closure to the MN4 cluster, so it can run there.
+    # Take a look at ${program}
+    # to see what is being executed.
+
+    nixtools=${nixPrefix}${nixtools}/bin
+    runexp=\$nixtools/${targetCluster}/runexp
+
+    >&2 echo "Launching \"\$runexp ${program}\" in MN4"
+    ssh ${sshHost} \$runexp ${program}
+    EOF
     chmod +x $out
   '';
 }
