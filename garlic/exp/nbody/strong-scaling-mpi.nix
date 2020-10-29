05ce36e1586fd9a4058469f6a004a45e438bf6f6
Antoni Navarro <anavarr1@bsc.es>
2020-10-29 16:31:21 +0100

    Add the MPI-weak scaling experiment and strong scaling experiments

---
+++ garlic/exp/nbody/strong-scaling-mpi.nix
@@ -0,0 +1,62 @@
+{
+  stdenv
+, stdexp
+, bsc
+, targetMachine
+, stages
+}:
+
+with stdenv.lib;
+
+let
+  # Initial variable configuration
+  varConf = with bsc; {
+    numProcsAndParticles = [ 1 2 4 8 16 32 48 ];
+  };
+
+  # Generate the complete configuration for each unit
+  genConf = with bsc; c: targetMachine.config // rec {
+    # nbody options
+    inherit (c) numProcsAndParticles;
+    particles = 1024 * numProcsAndParticles * 2;
+    timesteps = 10;
+    blocksize = 1024;
+    cc = icc;
+    mpi = impi;
+    gitBranch = "garlic/mpi+send";
+
+    # Repeat the execution of each unit 30 times
+    loops = 30;
+
+    # Resources
+    qos = "debug";
+    ntasksPerNode = numProcsAndParticles;
+    nodes = 1;
+    time = "02:00:00";
+    cpuBind = "sockets,verbose";
+    jobName = "nbody-bs-${toString numProcsAndParticles}-${gitBranch}";
+  };
+
+  # Compute the array of configurations
+  configs = stdexp.buildConfigs {
+    inherit varConf genConf;
+  };
+
+  exec = {nextStage, conf, ...}: with conf; stages.exec {
+    inherit nextStage;
+    argv = [ "-t" timesteps "-p" particles ];
+  };
+
+  program = {nextStage, conf, ...}: with conf;
+  let
+    customPkgs = stdexp.replaceMpi conf.mpi;
+  in
+    customPkgs.apps.nbody.override {
+      inherit cc blocksize mpi gitBranch;
+    };
+
+  pipeline = stdexp.stdPipeline ++ [ exec program ];
+
+in
+
+  stdexp.genExperiment { inherit configs pipeline; }
