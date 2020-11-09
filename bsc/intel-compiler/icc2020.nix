acc3390b6ba720ff4a9791f6673b79564905d34d
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-11-09 13:08:45 +0100

    WIP: icc update

--- bsc/intel-compiler/icc2020.nix
+++ bsc/intel-compiler/icc2020.nix
@@ -16,17 +16,20 @@ stdenv.mkDerivation rec {
   };
 
   # From Arch Linux PKGBUILD
-  dir_nr="16526";
+  dir_nr="17114";
   year="2020";
   v_a="1";
   v_b="217";
-  update="1";
+  update="4";
   composer_xe_dir="compilers_and_libraries_${year}.${v_a}.${v_b}";
-  tgz="parallel_studio_xe_2020_update${update}_cluster_edition.tgz";
+  #tgz="parallel_studio_xe_2020_update${update}_cluster_edition.tgz";
+  tgz="parallel_studio_xe_2020_update${update}_professional_edition.tgz";
+
+  #https://registrationcenter-download.intel.com/akdlm/irc_nas/tec/17114/parallel_studio_xe_2020_update4_professional_edition.tgz
   
   src = fetchurl {
     url = "http://registrationcenter-download.intel.com/akdlm/IRC_NAS/tec/${dir_nr}/${tgz}";
-    sha256 = "1axblai5lmw9yqjaz7lvjraj5fsc7r37pklb9x3n1gdjfbgdh4gx";
+    sha256 = "0nmp6np4s7nx2p94x40bpqkp5nasgif3gmbfl4lajzgj2rkh871v";
   };
 
   buildInputs = [
@@ -41,6 +44,8 @@ stdenv.mkDerivation rec {
   propagatedBuildInputs = [ gcc ];
 
   installPhase = ''
+    pwd
+    ls -l rpm
     rpmextract rpm/intel-icc-*.rpm
     rpmextract rpm/intel-comp-*.rpm
     rpmextract rpm/intel-c-comp-*.rpm
