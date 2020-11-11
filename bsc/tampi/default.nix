1838178761595c9c7320dba4253c7de0b1cf4b36
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-11-11 12:24:41 +0100

    tampi: Update to 1.0.2 and use fetchFromGitHub
    The configure flags are no longer required.

--- bsc/tampi/default.nix
+++ bsc/tampi/default.nix
@@ -1,6 +1,6 @@
 {
   stdenv
-, fetchurl
+, fetchFromGitHub
 , automake
 , autoconf
 , libtool
@@ -12,14 +12,16 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.0.1";
-  name = "tampi-${version}";
+  version = "1.0.2";
+  pname = "tampi";
   enableParallelBuilding = true;
   buildInputs = [ autoreconfHook automake autoconf libtool gnumake boost mpi gcc ];
   dontDisableStatic = true;
-  configureFlags = [ "--disable-mpi-mt-check" "CXXFLAGS=-DOMPI_SKIP_MPICXX=1" ];
-  src = fetchurl {
-    url = "https://github.com/bsc-pm/tampi/archive/v${version}.tar.gz";
-    sha256 = "8608a74325939d2a6b56e82f7f6788efbc67731e2d64793bac69475f5b4b9704";
+
+  src = fetchFromGitHub {
+    owner = "bsc-pm";
+    repo = "tampi";
+    rev = "v${version}";
+    sha256 = "09711av3qbah56mchr81679x05zxl3hi0pjndcnvk7jsfcdxvbm7";
   };
 }
