d2d3ccf332d6d09e4a90c02e0c27d22aa6d03f73
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-11-17 18:33:57 +0100

    Idea for FS naming convention

--- garlic/machines.nix
+++ garlic/machines.nix
@@ -18,6 +18,18 @@
         cachelineBytes = 64;
       };
     };
+
+    # Experimental naming convention for the FS
+    #fs = {
+    #  cluster = {
+    #    fast = "/gpfs/scratch/bsc15/bsc15557/garlic";
+    #    reliable = "/gpfs/projects/bsc15/garlic";
+    #  };
+    #  node = {
+    #    fast = "$TMPDIR";
+    #  };
+    #};
+
     # TODO: Add the specific details for SLURM and the interconection here
   };
 }
