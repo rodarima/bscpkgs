2863ab6ae1ef00069bdbe6f8eb6bbc2070edae15
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-11-20 15:29:03 +0100

    machines: Use fs topology

--- garlic/machines.nix
+++ garlic/machines.nix
@@ -20,15 +20,28 @@
     };
 
     # Experimental naming convention for the FS
-    #fs = {
-    #  cluster = {
-    #    fast = "/gpfs/scratch/bsc15/bsc15557/garlic";
-    #    reliable = "/gpfs/projects/bsc15/garlic";
-    #  };
-    #  node = {
-    #    fast = "$TMPDIR";
-    #  };
-    #};
+    fs = rec {
+      topology = {
+        gpfs = {
+          projects = "/gpfs/projects/bsc15/garlic";
+          scratch = "/gpfs/scratch/bsc15/bsc15557/garlic";
+        };
+
+        ssd = {
+          # Beware to expand the temp dir at execution time
+          temp = "$TMPDIR";
+        };
+      };
+
+      shared = with topology; {
+        fast = gpfs.scratch;
+        reliable = gpfs.projects;
+      };
+
+      local = {
+        temp = topology.ssd.temp;
+      };
+    };
 
     # TODO: Add the specific details for SLURM and the interconection here
   };
