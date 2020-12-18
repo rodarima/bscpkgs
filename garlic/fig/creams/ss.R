afd333adef729b6d2a1d2e004855dcb4f5da8deb
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2020-12-18 12:43:06 +0100

    creams: fix indentation

--- garlic/fig/creams/ss.R
+++ garlic/fig/creams/ss.R
@@ -11,11 +11,11 @@ if (length(args)>0) { input_file = args[1] }
 
 # Load the dataset in NDJSON format
 dataset = jsonlite::stream_in(file(input_file), verbose=FALSE) %>%
-	jsonlite::flatten()
+  jsonlite::flatten()
 
 # We only need some colums
 df = select(dataset, unit, config.nodes, config.gitBranch, time) %>%
-	rename(nodes=config.nodes, gitBranch=config.gitBranch)
+  rename(nodes=config.nodes, gitBranch=config.gitBranch)
 
 df$unit = as.factor(df$unit)
 df$nnodes = df$nodes
@@ -27,23 +27,23 @@ levels(df$gitBranch) <- substring((levels(df$gitBranch)), 8)
 
 # Compute new columns
 D=group_by(df, unit) %>%
-	mutate(tnorm = time / median(time) - 1) %>%
-	mutate(bad = ifelse(max(abs(tnorm)) >= 0.01, 1, 0)) %>%
+  mutate(tnorm = time / median(time) - 1) %>%
+  mutate(bad = ifelse(max(abs(tnorm)) >= 0.01, 1, 0)) %>%
   mutate(variability = ifelse(bad > 0, "large", "ok")) %>%
-	mutate(mtime = median(time)) %>%
-	mutate(nmtime = mtime*nnodes) %>%
-	mutate(ntime = time*nnodes) %>%
+  mutate(mtime = median(time)) %>%
+  mutate(nmtime = mtime*nnodes) %>%
+  mutate(ntime = time*nnodes) %>%
   ungroup() %>%
   mutate(min_nmtime = min(nmtime)) %>%
   mutate(rnmtime = nmtime / min_nmtime) %>%
   mutate(rntime = ntime / min_nmtime) %>%
   mutate(rmeff = 1.0 / rnmtime) %>%
   mutate(reff = 1.0 / rntime) %>%
-	group_by(gitBranch) %>%
-	mutate(tmax = max(mtime)) %>%
-	mutate(speedup=tmax/time) %>%
-	mutate(eff=speedup/nnodes) %>%
-	mutate(mspeedup=tmax/mtime) %>%
+  group_by(gitBranch) %>%
+  mutate(tmax = max(mtime)) %>%
+  mutate(speedup=tmax/time) %>%
+  mutate(eff=speedup/nnodes) %>%
+  mutate(mspeedup=tmax/mtime) %>%
   mutate(meff=mspeedup/nnodes) %>%
   ungroup()
 
@@ -56,32 +56,32 @@ w=5
 
 png("variability.png", width=1.5*w*ppi, height=h*ppi, res=ppi)
 p = ggplot(data=D, aes(x=nodes, y=tnorm, color=variability)) +
-	theme_bw() +
-	theme(plot.subtitle=element_text(size=8)) +
-	# Add the maximum allowed error lines
-	geom_hline(yintercept=c(-0.01, 0.01),
-		linetype="dashed", color="gray") +
-	# Draw boxplots
-	geom_boxplot(aes(fill=gitBranch)) +
-	scale_color_manual(values=c("brown", "black")) +
-	# Labels
-	labs(x="Nodes", y="Normalized time", title="Creams strong scaling",
+  theme_bw() +
+  theme(plot.subtitle=element_text(size=8)) +
+  # Add the maximum allowed error lines
+  geom_hline(yintercept=c(-0.01, 0.01),
+    linetype="dashed", color="gray") +
+  # Draw boxplots
+  geom_boxplot(aes(fill=gitBranch)) +
+  scale_color_manual(values=c("brown", "black")) +
+  # Labels
+  labs(x="Nodes", y="Normalized time", title="Creams strong scaling",
     subtitle=input_file)
 print(p)
 dev.off()
 
 png("time.png", width=w*1.5*ppi, height=h*ppi, res=ppi)
 p = ggplot(D, aes(x=nodes, y=mtime, color=gitBranch)) +
-	theme_bw() +
-	theme(plot.subtitle=element_text(size=8)) +
-	geom_line(aes(group=gitBranch)) +
-	#geom_point() +
-	geom_point(aes(shape=variability), size=3) +
-	scale_shape_manual(values=c(21, 19)) +
+  theme_bw() +
+  theme(plot.subtitle=element_text(size=8)) +
+  geom_line(aes(group=gitBranch)) +
+  #geom_point() +
+  geom_point(aes(shape=variability), size=3) +
+  scale_shape_manual(values=c(21, 19)) +
   #  position=position_dodge(width=0.3)) +
-	#scale_x_continuous(trans=log2_trans()) +
-	scale_y_continuous(trans=log2_trans()) +
-	labs(x="Nodes", y="Time (s)",
+  #scale_x_continuous(trans=log2_trans()) +
+  scale_y_continuous(trans=log2_trans()) +
+  labs(x="Nodes", y="Time (s)",
     title="Creams strong scaling (lower is better)",
     subtitle=input_file)
 print(p)
@@ -89,18 +89,18 @@ dev.off()
 
 png("refficiency.png", width=w*1.5*ppi, height=h*ppi, res=ppi)
 p = ggplot(D, aes(x=nodes, y=rmeff, color=gitBranch)) +
-	theme_bw() +
-	theme(plot.subtitle=element_text(size=8)) +
-	geom_line(aes(group=gitBranch)) +
-	geom_point(aes(shape=variability), size=3) +
-	#geom_boxplot(aes(y=reff),
+  theme_bw() +
+  theme(plot.subtitle=element_text(size=8)) +
+  geom_line(aes(group=gitBranch)) +
+  geom_point(aes(shape=variability), size=3) +
+  #geom_boxplot(aes(y=reff),
   #  position=position_dodge(width=0.0)) +
-	scale_shape_manual(values=c(21, 19)) +
-	#geom_point(aes(y=rntime),
+  scale_shape_manual(values=c(21, 19)) +
+  #geom_point(aes(y=rntime),
   #  position=position_dodge(width=0.3)) +
-	#scale_x_continuous(trans=log2_trans()) +
-	#scale_y_continuous(trans=log2_trans()) +
-	labs(x="Nodes", y="Relative efficiency (to best)",
+  #scale_x_continuous(trans=log2_trans()) +
+  #scale_y_continuous(trans=log2_trans()) +
+  labs(x="Nodes", y="Relative efficiency (to best)",
     title="Creams strong scaling (higher is better)",
     subtitle=input_file)
 print(p)
