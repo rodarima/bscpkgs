140598a28b4a34c973aab643c12c50bda7493e61
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2021-01-11 16:41:56 +0100

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,17 @@
 let
   bscOverlay = import ./overlay.nix;
-  pkgs = import <nixpkgs> {
+
+  # Pin the nixpkgs
+  nixpkgsPath = builtins.fetchTarball {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-20.09";
+    # Commit hash for nixos-20.09 as of 2021-01-11
+    url = "https://github.com/nixos/nixpkgs/archive/41dddb1283733c4993cb6be9573d5cef937c1375.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "1blbidbmxhaxar2x76nz72bazykc5yxi0algsbrhxgrsvijs4aiw";
+  };
+
+  pkgs = import nixpkgsPath {
     overlays = [ bscOverlay ];
   };
 
