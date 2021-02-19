a2306eb9419eecf06fc8e2bed02136a56df78e33
Sandra <smacia@bsc.es>
2021-02-19 11:07:58 +0100

    saiph: add some blocking experiments

---
+++ garlic/exp/saiph/blocking_ZY.nix
@@ -0,0 +1,69 @@
+{
+  stdenv
+, stdexp
+, bsc
+, targetMachine
+, stages
+}:
+
+with stdenv.lib;
+
+let
+  # Initial variable configuration
+  varConf = with bsc; {
+    nb = [ 4 8 16 32 64 128 ];
+  };
+
+  # Generate the complete configuration for each unit
+  genConf = with bsc; c: targetMachine.config // rec {
+    expName = "saiph.blockingZY";
+    unitName = "${expName}.nbx-nby-nbz-${toString nbx}-${toString nby}-${toString nbz}";
+    inherit (targetMachine.config) hw;
+
+    # saiph options
+    nbx = 1;
+    nby = c.nb;
+    nbz = 8;
+    mpi = impi;
+    gitBranch = "garlic/tampi+isend+oss+task+simd";
+
+    # Repeat the execution of each unit 50 times
+    loops = 30;
+
+    # Resources
+    cachelineBytes = hw.cachelineBytes;
+    qos = "debug";
+    time = "01:00:00";
+    nodes = 1;
+    ntasksPerNode = hw.socketsPerNode;
+    cpusPerTask = hw.cpusPerSocket;
+    jobName = "${unitName}-${gitBranch}";
+  };
+
+  # Compute the array of configurations
+  configs = stdexp.buildConfigs {
+    inherit varConf genConf;
+  };
+
+  exec = {nextStage, conf, ...}: with conf; stages.exec {
+    inherit nextStage;
+    env = ''
+      export NANOS6_REPORT_PREFIX="#"
+      export I_MPI_THREAD_SPLIT=1
+      export ASAN_SYMBOLIZER_PATH=${bsc.clangOmpss2Unwrapped}/bin/llvm-symbolizer
+    '';
+  };
+
+  program = {nextStage, conf, ...}: with conf;
+  let
+    customPkgs = stdexp.replaceMpi conf.mpi;
+  in
+    customPkgs.apps.saiph.override {
+      inherit nbx nby nbz mpi gitBranch cachelineBytes;
+    };
+
+  pipeline = stdexp.stdPipeline ++ [ exec program ];
+
+in
+ 
+  stdexp.genExperiment { inherit configs pipeline; }
