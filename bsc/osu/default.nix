c684b1870a19db95ac71ed94049679a71c773242
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2021-03-03 17:41:05 +0100

    osu: update 5.6.3 -> 5.7

--- bsc/osu/default.nix
+++ bsc/osu/default.nix
@@ -5,12 +5,12 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "5.6.3";
+  version = "5.7";
   name = "osu-micro-benchmarks-${version}";
 
   src = fetchurl {
     url = "http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-${version}.tar.gz";
-    sha256 = "1f5fc252c0k4rd26xh1v5017wfbbsr2w7jm49x8yigc6n32sisn5";
+    sha256 = "1425ygxpk3kyy6ilh4f6qjsjdyx0gjjzs7ic1cb7zjmn1vhfnw0l";
   };
 
   doCheck = true;
