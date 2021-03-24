72e7a8dab759b825845773998de7701d711f5bcb
Sandra <smacia@bsc.es>
2021-03-24 09:07:54 +0100

    shell: add clangOmpss2 and gdb

--- garlic/shell.nix
+++ garlic/shell.nix
@@ -10,7 +10,7 @@ mkShell {
     # Packages from garlic
     (with garlic; [ tool garlicd ]) ++
     # Packages from bsc
-    [ groff paraver icc nix openssh git cn6 nix-diff ];
+    [ groff paraver icc nix openssh git cn6 nix-diff clangOmpss2 gdb ];
 
   # inputsFrom to get build dependencies
 
