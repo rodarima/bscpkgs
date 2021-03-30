b7dcf7bc696aad395b364ace5cbe5f0afc982ce6
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2021-03-30 16:35:47 +0200

    rplot: add support for gziped datasets

--- garlic/pp/rplot.nix
+++ garlic/pp/rplot.nix
@@ -31,9 +31,16 @@ in stdenv.mkDerivation {
     export FONTCONFIG_PATH=${fontconfig.out}/etc/fonts
     mkdir -p $out
     cd $out
-    ln -s ${dataset} input
+    dataset="${dataset}"
+
+    ln -s $dataset input
     Rscript --vanilla ${script} ${dataset}
-    jq -c .total_time input |\
+
+    if [ "''${dataset##*.}" == gz ]; then
+      gunzip --stdout $dataset
+    else
+      cat $dataset
+    fi | jq -c .total_time |\
       awk '{s+=$1} END {printf "%f\n", s/60}' > total_job_time_minutes
   '';
 }
