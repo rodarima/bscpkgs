ef5e98e06d395a578bfb5bcd1a0efdc892c02050
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2021-06-16 19:30:43 +0200

    icc2021: fix wrapper names and ldflags

--- bsc/intel-compiler/wrapper2021.nix
+++ bsc/intel-compiler/wrapper2021.nix
@@ -14,13 +14,14 @@ in wrapCCWith rec {
     echo "-B${gcc.cc}/lib/gcc/${targetConfig}/${gcc.version}" >> $out/nix-support/cc-cflags
     echo "-isystem ${iccUnwrapped}/include" >> $out/nix-support/cc-cflags
     echo "-isystem ${iccUnwrapped}/include/icc" >> $out/nix-support/cc-cflags
+
+    echo "-L${iccUnwrapped}/lib" >> $out/nix-support/cc-ldflags
     echo "-L${gcc.cc}/lib/gcc/${targetConfig}/${gcc.version}" >> $out/nix-support/cc-ldflags
     echo "-L${gcc.cc.lib}/lib" >> $out/nix-support/cc-ldflags
 
-    # Create the wrappers for icc and icpc
+    # Create the wrappers for icx*
+    wrap lld  $wrapper $ccPath/lld
     wrap icx  $wrapper $ccPath/icx
-    wrap icx  $wrapper $ccPath/icpx
-    wrap icx  $wrapper $ccPath/clang
-    wrap icx  $wrapper $ccPath/clang++
+    wrap icpx  $wrapper $ccPath/icpx
   '';
 }
