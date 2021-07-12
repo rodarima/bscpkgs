762fe8b82c120e4bec762f3d469c2d599c8484f9
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2021-07-12 18:32:17 +0200

    wxpropgrid: disable parallel build
    The parallel build fails with high probability due to incorrect
    dependencies in the Makefile, which lead to an attemp to link too soon.

--- bsc/wxpropgrid/default.nix
+++ bsc/wxpropgrid/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     sha256 = "1f62468x5s4h775bn5svlkv0lzzh06aciljpiqn5k3w2arkaijgh";
   };
 
-  enableParallelBuilding = true;
+  enableParallelBuilding = false;
 
   buildInputs = [
     wx
