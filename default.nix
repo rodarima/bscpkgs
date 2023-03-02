38220140ec8f874e4ed2aa3da4442ac6375da38b
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-03-02 11:16:54 +0100

    Update nixpkgs to release 22.11

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
 let
   bscOverlay = import ./overlay.nix;
 
-  commit = "1614b96a68dd210919865abe78bda56b501eb1ef";
+  commit = "3c0a90afd70b46b081601f9941999e596576337f";
 
   # Pin the nixpkgs
   nixpkgsPath = builtins.fetchTarball {
     # Descriptive name to make the store path easier to identify
     name = "nixpkgs-${commit}";
-    # Commit hash for nixpkgs as of 2021-09-01
+    # Commit hash for nixpkgs release-22.11 as of 2023-03-02
     url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "17c4a6cg0xmnp6hl76h8fvxglngh66s8nfm3qq2iqv6iay4a92qz";
+    sha256 = "0ss4cigiph1ck4lr0qjiw79pjsi4q0nd00mjfzmzmarxdphjsmyy";
   };
 
   pkgs = import nixpkgsPath {
