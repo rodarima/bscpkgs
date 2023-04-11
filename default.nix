a46a2ee7940e1793a740ff4b2bb6db1c3551dcd2
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-04-11 20:31:54 +0200

    Update nixpkgs commit to match xeon07

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
 let
   bscOverlay = import ./overlay.nix;
 
-  commit = "3c0a90afd70b46b081601f9941999e596576337f";
+  commit = "9a6aabc4740790ef3bbb246b86d029ccf6759658";
 
   # Pin the nixpkgs
   nixpkgsPath = builtins.fetchTarball {
     # Descriptive name to make the store path easier to identify
     name = "nixpkgs-${commit}";
-    # Commit hash for nixpkgs release-22.11 as of 2023-03-02
+    # Commit hash for nixpkgs as of 2023-04-11
     url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0ss4cigiph1ck4lr0qjiw79pjsi4q0nd00mjfzmzmarxdphjsmyy";
+    sha256 = "0m8cid2n6zfnbia7kkan9vw8n5dvwn8sv7cmrap46rckpzbahnls";
   };
 
   pkgs = import nixpkgsPath {
