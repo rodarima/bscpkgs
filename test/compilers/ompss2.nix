bb6129a77e4d63d666053664c91a89feeb357f79
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-05-22 13:55:23 +0200

    Reduce ompss2 test verbosity

--- test/compilers/ompss2.nix
+++ test/compilers/ompss2.nix
@@ -23,19 +23,22 @@ stdenv.mkDerivation rec {
   dontUnpack = true;
   dontConfigure = true;
   hardeningDisable = [ "all" ];
-  NIX_DEBUG = 1;
+  #NIX_DEBUG = 1;
   buildInputs = [ strace gdb ];
   # NODES requires access to /sys/devices to request NUMA information. It will
   # fail to run otherwise, so we disable the sandbox for this test.
   __noChroot = true;
   buildPhase = ''
-    set -x
-    echo CC=$CC
-    $CC -v
+    #set -x
+    #$CC -v
 
     cp ${task_c} task.c
+
+    echo CC=$CC
+    echo NANOS6_HOME=$NANOS6_HOME
+    echo NODES_HOME=$NODES_HOME
     cat task.c
-    $CC -v -fompss-2 task.c -o task
+    $CC -fompss-2 task.c -o task
     #strace -ff -e trace=open,openat -s9999999 ./task
     LD_DEBUG=libs ./task
     #gdb -batch -ex "run" -ex "bt" ./task
