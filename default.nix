7f18deaf69b2167a3e4b28147e4f9b69e9f85df4
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-06-16 16:36:16 +0200

    Update nixpkgs to d6b863fd to match nixos

--- default.nix
+++ default.nix
@@ -1,16 +1,15 @@
 let
   bscOverlay = import ./overlay.nix;
 
-  commit = "9a6aabc4740790ef3bbb246b86d029ccf6759658";
+  commit = "d6b863fd9b7bb962e6f9fdf292419a775e772891";
 
   # Pin the nixpkgs
   nixpkgsPath = builtins.fetchTarball {
     # Descriptive name to make the store path easier to identify
     name = "nixpkgs-${commit}";
-    # Commit hash for nixpkgs as of 2023-04-11
     url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0m8cid2n6zfnbia7kkan9vw8n5dvwn8sv7cmrap46rckpzbahnls";
+    sha256 = "02rd1n6d453rdp2978bvnp99nyfa26jxgbsg78yb9mmdxvha3hnr";
   };
 
   pkgs = import nixpkgsPath {
