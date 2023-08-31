c30851d6e90458ba2707520bdc1ca14b66acd429
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-08-31 12:40:54 +0200

    Add packages to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,19 @@
 {
-  outputs = {...}: {
-    bscOverlay = import ./overlay.nix;
+  inputs = {
+    nixpkgs.url = "nixpkgs";
   };
+
+  outputs = { self, nixpkgs, ...}:
+  let
+    pkgs = import nixpkgs {
+      # For now we only support x86
+      system = "x86_64-linux";
+      overlays = [ self.overlays.default ];
+    };
+  in
+    {
+      bscOverlay = import ./overlay.nix;
+      overlays.default = self.bscOverlay;
+      packages.x86_64-linux = pkgs;
+    };
 }
