4883b750bd2edcc9c43c458a8c9bd46cb94f68c4
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-09-07 09:08:36 +0200

    Fix bench6 commit

--- bsc/bench6/default.nix
+++ bsc/bench6/default.nix
@@ -8,6 +8,7 @@
 , tampiGit
 , gitBranch ? "master"
 , gitURL ? "ssh://git@bscpm03.bsc.es/rarias/bench6.git"
+, gitCommit ? "1e6ce2aa8ad7b4eef38df1581d7ec48a8815f85d"
 }:
 
 stdenv.mkDerivation rec {
@@ -17,6 +18,7 @@ stdenv.mkDerivation rec {
   src = builtins.fetchGit {
     url = gitURL;
     ref = gitBranch;
+    rev = gitCommit;
   };
 
   buildInputs = [ cmake clangOmpss2Git nanos6Git nodes mpi tampiGit ];
