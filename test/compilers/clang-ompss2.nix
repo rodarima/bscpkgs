ff34ab5732885a8279b5d6ee1e1d55e3ab6c57f2
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-11-21 17:01:32 +0100

    Enable NIX_DEBUG = 1 in the clang-ompss2 test
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- test/compilers/clang-ompss2.nix
+++ test/compilers/clang-ompss2.nix
@@ -5,9 +5,9 @@ stdenv.mkDerivation rec {
   name = "test-clang-ompss2";
   src = ./.;
   buildInputs = [ clang-ompss2 nanos6 ];
+  NIX_DEBUG = 1;
 
   buildPhase = ''
-    #export NIX_DEBUG=6
     clang -fompss-2 hello.c -o hello
     ./hello
     clang++ -fompss-2 hello.cc -o hello
