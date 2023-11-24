8a31895e48ac36a21444381ed7568dc8d8f0d958
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2023-11-24 15:22:49 +0100

    Update nixpkgs commit in default.nix
    Reviewed-By: Rodrigo Arias Mallo <rodrigo.arias@bsc.es>

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,9 +1,9 @@
 let
-  commit = "d680ded26da5cf104dd2735a51e88d2d8f487b4d";
+  commit = "e4ad989506ec7d71f7302cc3067abd82730a4beb";
 in builtins.fetchTarball {
     # Descriptive name to make the store path easier to identify
     name = "nixpkgs-${commit}";
     url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0xczslr40zy1wlg0ir8mwyyn5gz22i2f9dfd0vmgnk1664v4chky";
+    sha256 = "sha256-de9KYi8rSJpqvBfNwscWdalIJXPo8NjdIZcEJum1mH0=";
 }
