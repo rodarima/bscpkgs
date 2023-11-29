fec4ddf6abf63e1928366d9e5d3363093a1eb3f3
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-11-29 16:59:19 +0100

    Merge TAMPI release and git in the same file
    In order to reduce duplicate information we just place the two sources
    in the same file.
    
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- pkgs/tampi/default.nix
+++ pkgs/tampi/default.nix
@@ -9,22 +9,46 @@
 , mpi
 , gcc
 , autoreconfHook
+, useGit ? false
+, gitUrl ? "ssh://git@bscpm03.bsc.es/interoperability/tampi.git"
+, gitBranch ? "master"
+, gitCommit ? "16f92094ca6da25e2f561c000f5fbc2901944f7b"
 }:
 
-stdenv.mkDerivation rec {
-  version = "3.0";
+let
+  release = rec {
+    version = "3.0";
+    src = fetchFromGitHub {
+      owner = "bsc-pm";
+      repo = "tampi";
+      rev = "v${version}";
+      hash = "sha256-qdWBxPsXKM428F2ojt2B6/0ZsQyGzEiojNaqbhDmrks=";
+    };
+  };
+  git = rec {
+    version = src.shortRev;
+    src = builtins.fetchGit {
+      url = gitUrl;
+      ref = gitBranch;
+      rev = gitCommit;
+    };
+  };
+  source = if (useGit) then git else release;
+in stdenv.mkDerivation rec {
   pname = "tampi";
+  inherit (source) src version;
   enableParallelBuilding = true;
   separateDebugInfo = true;
-  buildInputs = [ autoreconfHook automake autoconf libtool gnumake boost mpi gcc ];
+  buildInputs = [
+    autoreconfHook
+    automake
+    autoconf
+    libtool
+    gnumake
+    boost
+    mpi
+    gcc
+  ];
   dontDisableStatic = true;
-
-  src = fetchFromGitHub {
-    owner = "bsc-pm";
-    repo = "tampi";
-    rev = "v${version}";
-    hash = "sha256-qdWBxPsXKM428F2ojt2B6/0ZsQyGzEiojNaqbhDmrks=";
-  };
-
   hardeningDisable = [ "all" ];
 }
