867ba3ec5a546c25fc2640760b1ca717856e8c65
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2025-07-18 10:45:30 +0200

    Update nOS-V to 3.2.0
    Reviewed-by: Aleix Boné <abonerib@bsc.es>

--- pkgs/nosv/default.nix
+++ pkgs/nosv/default.nix
@@ -11,19 +11,19 @@
 , useGit ? false
 , gitUrl ? "git@gitlab-internal.bsc.es:nos-v/nos-v.git"
 , gitBranch ? "master"
-, gitCommit ? "cfd361bd1dd30c96da405e6bbaa7e78f5f93dfda"
+, gitCommit ? "9f47063873c3aa9d6a47482a82c5000a8c813dd8"
 }:
 
 with lib;
 
 let
   release = rec {
-    version = "3.1.0";
+    version = "3.2.0";
     src = fetchFromGitHub {
       owner = "bsc-pm";
       repo = "nos-v";
       rev = "${version}";
-      hash = "sha256-Pkre+ZZsREDxJLCoIoPN1HQDuUa2H1IQyKB3omg6qaU=";
+      hash = "sha256-yaz92426EM8trdkBJlISmAoG9KJCDTvoAW/HKrasvOw=";
     };
   };
 
